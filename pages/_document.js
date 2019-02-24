30f6290d69a412c207fe6a7a42a25cb42b256a46
junlee91 <jlee757@uic.edu>
2019-02-23 19:57:56 -0600

    Adding ant design lib

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
@@ -11,7 +11,13 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta name="CareerTalk Team" content={"CareerTalk Stats"} />
+          <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.13.6/antd.min.css"
+            rel="stylesheet"
+          />
           <link href="/static/styles.css" rel={"stylesheet"} />
+          <meta name="theme-color" content="black" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
           <Main />
