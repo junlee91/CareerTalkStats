a4ce81e01e5c98aa535d8415387db63f084ddb4f
junlee91 <jlee757@uic.edu>
2019-02-23 19:09:53 -0600

    Head title added

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,10 @@
-export default () => <h1>Hello from index</h1>;
+import Head from "next/head";
+
+export default () => (
+  <div>
+    <Head>
+      <title>Home | CareerTalk</title>
+    </Head>
+    <h1>Hello from index</h1>
+  </div>
+);
