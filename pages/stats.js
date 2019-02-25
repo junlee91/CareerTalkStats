379666e7b83ca3c11f271a23b2d34ba9c7b36696
junlee91 <jlee757@uic.edu>
2019-02-24 19:44:26 -0600

    chartjs base implementation

--- pages/stats.js
+++ pages/stats.js
@@ -1,10 +1,38 @@
 import Head from 'next/head';
+import { Row, Col } from 'antd';
+import { Doughnut, Pie } from 'react-chartjs-2';
+
+const data = {
+  datasets: [
+    {
+      data: [10, 20, 30],
+      backgroundColor: ['#e74c3c', '#f1c40f', '#2980b9']
+    }
+  ],
+
+  // These labels appear in the legend and in the tooltips when hovering different arcs
+  labels: ['Red', 'Yellow', 'Blue']
+};
+
+const options = {
+  legend: {
+    position: 'bottom'
+  }
+};
 
 export default () => (
   <div>
     <Head>
       <title>Stats | CareerTalk</title>
     </Head>
-    <h1>This is Stats</h1>
+    <h1>CareerTalk Stats</h1>
+    <Row gutter={40}>
+      <Col key="1" span={12}>
+        <Doughnut data={data} options={options} />
+      </Col>
+      <Col key="2" span={12}>
+        <Pie data={data} />
+      </Col>
+    </Row>
   </div>
 );
