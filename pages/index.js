f4a93b7af5bd858a6cd8d0ed6a746f9f45fd1143
junlee91 <jlee757@uic.edu>
2019-03-03 17:21:00 -0600

    Homepage layout

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,60 @@ export default () => (
     <Head>
       <title>Home | CareerTalk</title>
     </Head>
-    <h1>This is Home</h1>
+    <div className="home-content">
+      <div className="content">
+        <div className="home-logo-box">
+          <img src="/static/images/ignite_logo.png" alt="CareerTalk" />
+        </div>
+      </div>
+      <div className="market-content">
+        <a href="https://play.google.com/store/apps/details?id=com.careertalk" target="_">
+          <img src="/static/images/google_play.png" alt="Google Play" />
+        </a>
+        <a href="https://itunes.apple.com/us/app/careertalk-find-your-jobs/id1435448112" target="_">
+          <img src="/static/images/app_store.png" alt="Google Play" />
+        </a>
+      </div>
+    </div>
+    <style jsx>
+      {`
+        .home-content {
+          padding: 10px 60px;
+        }
+
+        .content {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, 1fr);
+          width: 100%;
+        }
+
+        .home-logo-box {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .home-logo-box img {
+          width: 100%;
+          max-width: 450px;
+          height: auto;
+        }
+
+        .market-content {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+        }
+
+        .market-content img {
+          width: 100%;
+          max-width: 300px;
+          height: auto;
+          margin: -25px 0px -25px 0px;
+        }
+      `}
+    </style>
   </div>
 );
