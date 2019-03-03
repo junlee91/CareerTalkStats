c432ef2d2183e850238b16dc088f45c395b248e2
junlee91 <jlee757@uic.edu>
2019-03-03 12:55:59 -0600

    Google Sigin user horizontal bar added + color fix + layout fix

--- pages/stats.js
+++ pages/stats.js
@@ -1,41 +1,87 @@
 import Head from 'next/head';
-import { Bar, Pie } from 'react-chartjs-2';
+import { Bar, HorizontalBar, Pie } from 'react-chartjs-2';
 
+/**
+ * Bar Data
+ */
 const data = {
   labels: ['IOS', 'Android'],
   datasets: [
     {
-      data: [339, 226],
-      backgroundColor: ['#1eb1f9', '#76c159']
+      data: [479, 319],
+      backgroundColor: ['#1bc1fb', '#76c159']
     }
   ]
 };
 
 const barData = {
-  labels: ['Past Career Fair', '2/13 Career Fair'],
+  labels: ['Last Career Fair', '2/13 Career Fair'],
   datasets: [
     {
       label: 'Downloads',
-      backgroundColor: ['#3e95cd', '#8e5ea2'],
+      backgroundColor: ['#f6e58d', '#f39c12'],
       data: [233, 565]
     }
   ]
 };
 
+const horizontalBarData = {
+  labels: ['Assets'],
+
+  datasets: [
+    {
+      label: ['Google User'],
+      data: [520],
+      backgroundColor: '#29a645'
+    },
+    {
+      label: ['Default User'],
+      data: [45],
+      backgroundColor: '#D3D3D3'
+    }
+  ]
+};
+
+/**
+ * Bar Options
+ */
 const options = {
   legend: {
     display: false
   },
   maintainAspectRatio: false,
-  responsive: false
 };
 
 const barOptions = {
   legend: {
     display: false
   },
+  maintainAspectRatio: false
+};
+
+const horizontalBarOptions = {
+  tooltips: {
+    enabled: false
+  },
+  scales: {
+    xAxes: [
+      {
+        stacked: true
+      }
+    ],
+    yAxes: [
+      {
+        ticks: {
+          display: false
+        },
+        stacked: true
+      }
+    ]
+  },
+  legend: {
+    display: false
+  },
   maintainAspectRatio: false,
-  responsive: false
 };
 
 export default () => (
@@ -51,9 +97,9 @@ export default () => (
           <Pie data={data} options={options} width={350} height={350} />
         </div>
         <div style={{ marginTop: 15 }}>
-          <h2>Total downloads in 2/13 Career Fair: 565</h2>
-          <h3>IOS: 339</h3>
-          <h3>Android: 226</h3>
+          <h2>Total downloads: 798</h2>
+          <h3>IOS: 479</h3>
+          <h3>Android: 319</h3>
         </div>
       </div>
       <div className="box">
@@ -61,15 +107,23 @@ export default () => (
           <Bar data={barData} options={barOptions} width={350} height={350} />
         </div>
         <div style={{ marginTop: 15 }}>
-          <h2>Increased downloads</h2>
-          <h3>2/13 Fair: 565</h3>
-          <h3>Last Fair: 233</h3>
+          <h2>Downloads by fairs</h2>
+          <h3>2/13 Career Fair: 565</h3>
+          <h3>Last Career Fair: 233</h3>
         </div>
       </div>
       <div className="box">
+        <div style={{ width: 350, height: 100 }}>
+          <HorizontalBar
+            data={horizontalBarData}
+            options={horizontalBarOptions}
+            width={350}
+            height={350}
+          />
+        </div>
         <div style={{ marginTop: 15 }}>
-          <h2>Total downloads overall: 798</h2>
-          <h3>Google Sign in Users: 520</h3>
+          <h2>Google Sign in Users: 520</h2>
+          <h3>Total downloads in 2/13 Career Fair: 565</h3>
         </div>
       </div>
     </div>
@@ -87,6 +141,7 @@ export default () => (
           display: flex;
           flex-direction: column;
           align-items: center;
+          justify-content: center;
         }
       `}
     </style>
